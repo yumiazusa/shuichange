85b4469524390d62b28c0ef2cb600a317eb9ed44
yumiazusa <yumiazusa@hotmail.com>
2016-03-26 20:28:31 +0800

    first

---
+++ shuichangeApplication/Index/Controller/IndexController.class.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+namespace Index\Controller;
+use OT\DataDictionary;
+
+class IndexController extends HomeController {
+
+	//系统首页
+    public function index(){
+        $this->display();
+    }
+
+}
