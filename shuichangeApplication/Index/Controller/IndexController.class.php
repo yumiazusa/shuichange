4b4064682d27bd8931b9f33e0835b7c021ddbf72
yumiazusa <yumiazusa@hotmail.com>
2016-04-01 11:13:34 +0800

    weixin7

--- shuichangeApplication/Index/Controller/IndexController.class.php
+++ shuichangeApplication/Index/Controller/IndexController.class.php
@@ -38,7 +38,7 @@ class IndexController extends HomeController {
 		$getPageCounts = $newdb->where($where)->count();
         $pageSize = 15;
         $page = new \Think\Page($getPageCounts, $pageSize, $where);
-        $data = $newdb->where($where)->limit($page->firstRow, $page->listRows)
+        $data = $newdb->where($where)->order('create_time DESC')->limit($page->firstRow, $page->listRows)
                        ->select();
         $page->setConfig('prev','上一页');
         $page->setConfig('next','下一页');
