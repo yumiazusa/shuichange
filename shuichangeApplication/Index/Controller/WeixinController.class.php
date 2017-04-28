575d8f207bd4d931667572dc9e8e32db476f1862
yumiazusa <yumiazusa@hotmail.com>
2017-04-29 00:50:34 +0800

    9th

--- shuichangeApplication/Index/Controller/WeixinController.class.php
+++ shuichangeApplication/Index/Controller/WeixinController.class.php
@@ -52,7 +52,7 @@ class WeixinController extends HomeController {
 				$MsgType = "text";
 				$Content = 'lala';
 				$WechatAuth->sendText($data['FromUserName'], $Content);
-				
+				$response=$secret;
 				// $openid = $data['FromUserName'];
 				// $title = '文君首頁';
 				// $discription = '文君官方';
