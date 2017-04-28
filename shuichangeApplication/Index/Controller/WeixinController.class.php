626fe842f95f381424a9ec72ca3cd36688f1e9e1
yumiazusa <yumiazusa@hotmail.com>
2017-04-28 23:54:31 +0800

    9th

--- shuichangeApplication/Index/Controller/WeixinController.class.php
+++ shuichangeApplication/Index/Controller/WeixinController.class.php
@@ -16,13 +16,35 @@ class WeixinController extends HomeController {
 			$WechatAuth = new WechatAuth($appid, $secret, $access_token['access_token']);
 			/* 获取请求信息 */
 			$data = $wechat->request();
+			$Wx_user = M('wx_user');
 
 			$access_token['openid'] = $data['FromUserName'];
 
 			if ($data['Event'] == 'subscribe') {
+				$wx_user_id = $Wx_user->where(array('openid' => $data['FromUserName']))->getField('id');
+				if ($wx_user_id > 0) {
+					$userinfo = A('Api')->getUserinfo($data['FromUserName']);
+					$Wx_user->where(array('openid' => $data['FromUserName']))->save(array('subscribe' => 1, 'nickname' => $userinfo['nickname'], 'headimgurl' => $userinfo['headimgurl']));
+				}else{
+					$sceneArr = explode('_', $data['EventKey']);
+					if ($sceneArr[0] == 'qrscene') {
+						$save['scene'] = $sceneArr[2] ? $sceneArr[1] . '_' . $sceneArr[2] : $sceneArr[1]; //场景值  默认0
+					}
+					$userinfo = A('Api')->getUserinfo($data['FromUserName']);
+					$save['openid'] = $data['FromUserName'];
+					$save['nickname'] = $userinfo['nickname'];
+					$save['headimgurl'] = $userinfo['headimgurl'];
+					$save['sex'] = $userinfo['sex'];
+					$save['addtime'] = $userinfo['subscribe_time'];
+					$save['status'] = 1;
+					$save['user_id'] = 0;
+					$save['subscribe'] = 1;
+					$Wx_user_fans->add($save2);
+				}
 				$response=C('WEI_REPLAYWORD_SUBSCRIBE');
+
 			} else if ($data['Event'] == 'unsubscribe') {
-				
+				$Wx_user->where(array('openid' => $data['FromUserName']))->setField('subscribe', 0);
 			} else if ($data['Event'] == 'CLICK' && $data['EventKey'] == 'MENU_KEY_TEST') {
 				$userinfo = A('Api')->getUserinfo($data['FromUserName']);
 				// $response=json_encode($data);
