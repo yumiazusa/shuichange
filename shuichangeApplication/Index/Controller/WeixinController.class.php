a6c6a4868b8bf8b14b7a91213cbab188b8ee6831
yumiazusa <yumiazusa@hotmail.com>
2017-09-02 14:01:10 +0800

    9-1

--- shuichangeApplication/Index/Controller/WeixinController.class.php
+++ shuichangeApplication/Index/Controller/WeixinController.class.php
@@ -82,6 +82,7 @@ class WeixinController extends HomeController {
 					$response=json_encode($data);
 				}else if($data['MsgType'] == 'image'){
 					$WechatAuth->sendImage($data['FromUserName'], $data['MediaId']);
+					$response=json_encode($data);
 				}
 				// if(C('WEI_REPLAY_SWITCH')){
 				// 	$response=C('WEI_REPLAYWORD');
