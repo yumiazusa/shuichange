4f4193cf2965081eb233e6c2e517fdc6ccb7b01e
yumiazusa <yumiazusa@hotmail.com>
2017-09-02 14:10:36 +0800

    9-1

--- shuichangeApplication/Index/Controller/WeixinController.class.php
+++ shuichangeApplication/Index/Controller/WeixinController.class.php
@@ -81,7 +81,8 @@ class WeixinController extends HomeController {
 				}else if($data['Content'] == '测试'){
 					$response=json_encode($data);
 				}else if($data['MsgType'] == 'image'){
-					$WechatAuth->sendImage($data['FromUserName'], $data['MediaId']);
+					// $WechatAuth->sendImage($data['FromUserName'], $data['MediaId']);
+					$WechatAuth->sendText($data['FromUserName'], 'lala');
 					$response=json_encode($data);
 				}
 				// if(C('WEI_REPLAY_SWITCH')){
